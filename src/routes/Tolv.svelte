e756a038ce121bda3bbca8b3277cd7e08ff0604f
kjellevold <kamilla.kjellevold@gmail.com>
2020-05-05 13:25:07 +0200

    push

--- src/routes/Tolv.svelte
+++ src/routes/Tolv.svelte
@@ -9,14 +9,33 @@
 
 
 <header>
-
+    <button>12</button>
+    <button>Ansvarlig forbruk og produksjon</button>
 </header>
 <main>
-
+    <div id="homepage">
+        <div id="maalet">
+            <h1>Bærekraftsmålet</h1>
+        </div>
+        <div id="nyhet">
+            <h1>Artikkelside</h1>
+        </div>
+        <div id="funfact">
+            <h1>FunFacts</h1>
+        </div>
+    </div>
 </main>
 
 
 <style>
+    #homepage {
+        display: block;
+    }
 
+    #maalet {
+        border: 1px solid grey;
+        padding: 10px;
+        width: 100px;
+    }
 
 </style>
